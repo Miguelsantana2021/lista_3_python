a6aa966afd2d42f319dd4398e2857f874fa58c92
Miguel Marcelino <marcelinosantosmiguel1@gmail.com>
2023-08-27 13:58:54 -0300

    python

---
+++ lista3_ex1.py
@@ -0,0 +1,10 @@
+n1=int(input("digite o primeiro numero:"))
+n2=int(input("digite o segundo numero:"))
+if n1<n2:
+    while n1<n2:
+        print(n1+1)
+        n1=n1+1
+else:
+    while n2<n1:
+        print(n2+1)
+        n2=n2+1
